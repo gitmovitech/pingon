d3261562305e4bbb8c4faa3477db22f64084798e
Victor <victorandresv@gmail.com>
2017-03-18 12:40:29 -0300

    Primera pantalla de app

---
+++ app/src/main/java/cl/pingon/MainActivity.java
@@ -0,0 +1,18 @@
+package cl.pingon;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimary));
+        }
+    }
+}
