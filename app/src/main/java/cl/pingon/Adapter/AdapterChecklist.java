d688f49da84bc3413d110990b43626690c879aca
Victor <victorandresv@gmail.com>
2017-04-01 21:48:08 -0300

    text input layout

--- app/src/main/java/cl/pingon/Adapter/AdapterChecklist.java
+++ app/src/main/java/cl/pingon/Adapter/AdapterChecklist.java
@@ -2,11 +2,14 @@ package cl.pingon.Adapter;
 
 
 import android.content.Context;
+import android.support.design.widget.TextInputLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,6 +21,9 @@ public abstract class AdapterChecklist extends BaseAdapter {
     private Context context;
     private ArrayList<ModelChecklistFields> ChecklistFields;
     private int contador = 0;
+
+    private TextView Texto;
+    private EditText TextoInput;
 
     public AdapterChecklist(Context context, ArrayList<ModelChecklistFields> ChecklistFields){
         this.ChecklistFields = ChecklistFields;
@@ -41,17 +47,35 @@ public abstract class AdapterChecklist extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        Log.d("CAM_TIPO", ChecklistFields.get(contador).getCAM_TIPO());
-        contador++;
-        View ItemFechaView;
+
+        View ViewReturn;
+        LayoutInflater Inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         if(view == null) {
-            LayoutInflater Inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            ItemFechaView = Inflater.inflate(R.layout.item_fecha, null);
+            ViewReturn = Inflater.inflate(R.layout.item_empty, null);
         } else {
-            ItemFechaView = view;
+            ViewReturn = view;
         }
 
-        return ItemFechaView;
+        if(contador < getCount()){
+
+            switch(ChecklistFields.get(contador).getCAM_TIPO()){
+                case "texto":
+                    ViewReturn = Inflater.inflate(R.layout.item_texto, null);
+                    TextInputLayout TextoInput = (TextInputLayout) ViewReturn.findViewById(R.id.texto_input_layout);
+                    TextoInput.setHint(ChecklistFields.get(contador).getCAM_NOMBRE_INTERNO());
+                    break;
+                default:
+                    ViewReturn = Inflater.inflate(R.layout.item_title, null);
+                    TextView TextViewTitle = (TextView) ViewReturn.findViewById(R.id.TextViewTitle);
+                    TextViewTitle.setText(ChecklistFields.get(contador).getCAM_NOMBRE_INTERNO());
+                    Log.d("CAM_TIPO", ChecklistFields.get(contador).getCAM_TIPO());
+                    break;
+            }
+
+        }
+        contador++;
+
+        return ViewReturn;
     }
 }
