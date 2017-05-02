4adf7d48cf3fddfaf2a0469d8948538b5ff07955
Victor Vargas <Victor Vargas>
2017-05-02 18:15:37 -0300

    TODO task

---
+++ app/src/main/java/cl/pingon/Libraries/NetworkUtils.java
@@ -0,0 +1,41 @@
+package cl.pingon.Libraries;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+//TODO Chequear esto para detectar conexion wifi http://stackoverflow.com/questions/25678216/android-internet-connectivity-change-listener
+
+public class NetworkUtils {
+    public static int TYPE_WIFI = 1;
+    public static int TYPE_MOBILE = 2;
+    public static int TYPE_NOT_CONNECTED = 0;
+    public static final int NETWORK_STATUS_NOT_CONNECTED=0,NETWORK_STAUS_WIFI=1,NETWORK_STATUS_MOBILE=2;
+
+    public static int getConnectivityStatus(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (null != activeNetwork) {
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                return TYPE_WIFI;
+
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                return TYPE_MOBILE;
+        }
+        return TYPE_NOT_CONNECTED;
+    }
+
+    public static int getConnectivityStatusString(Context context) {
+        int conn = NetworkUtils.getConnectivityStatus(context);
+        int status = 0;
+        if (conn == NetworkUtils.TYPE_WIFI) {
+            status = NETWORK_STAUS_WIFI;
+        } else if (conn == NetworkUtils.TYPE_MOBILE) {
+            status =NETWORK_STATUS_MOBILE;
+        } else if (conn == NetworkUtils.TYPE_NOT_CONNECTED) {
+            status = NETWORK_STATUS_NOT_CONNECTED;
+        }
+        return status;
+    }
+}
